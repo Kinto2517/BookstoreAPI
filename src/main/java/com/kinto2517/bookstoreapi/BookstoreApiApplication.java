df50637169483f7a6b6adcdaead32baa4d4064c3
Kinto2517 <ersin.yilmaz2517@gmail.com>
2024-01-19 04:14:38 +0300

    Initial Commit

---
+++ src/main/java/com/kinto2517/bookstoreapi/BookstoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.kinto2517.bookstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApiApplication.class, args);
+	}
+
+}
