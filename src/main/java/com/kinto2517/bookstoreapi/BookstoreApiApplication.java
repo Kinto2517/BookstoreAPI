fedad28f4f37077cef1610a75163dd4e92e09810
Kinto2517 <ersin.yilmaz2517@gmail.com>
2024-01-19 07:53:11 +0300

    Enabling Scheduled Jobs

--- src/main/java/com/kinto2517/bookstoreapi/BookstoreApiApplication.java
+++ src/main/java/com/kinto2517/bookstoreapi/BookstoreApiApplication.java
@@ -2,8 +2,10 @@ package com.kinto2517.bookstoreapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+//@EnableScheduling
 public class BookstoreApiApplication {
 
 	public static void main(String[] args) {
