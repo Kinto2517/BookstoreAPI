a76df321b078547d81e026b53e74509145190a6b
Kinto2517 <ersin.yilmaz2517@gmail.com>
2024-01-19 04:12:15 +0300

    Token Entity for Clients

---
+++ src/main/java/com/kinto2517/bookstoreapi/token/Token.java
@@ -0,0 +1,37 @@
+package com.kinto2517.bookstoreapi.token;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import com.kinto2517.bookstoreapi.entity.User;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Token {
+
+  @Id
+  @GeneratedValue
+  public Long id;
+
+  @Column(unique = true)
+  public String token;
+
+  @Enumerated(EnumType.STRING)
+  public TokenType tokenType = TokenType.BEARER;
+
+  public boolean revoked;
+
+  public boolean expired;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  @JsonBackReference
+  public User user;
+}
---
+++ src/main/java/com/kinto2517/bookstoreapi/token/TokenType.java
@@ -0,0 +1,5 @@
+package com.kinto2517.bookstoreapi.token;
+
+public enum TokenType {
+  BEARER
+}
